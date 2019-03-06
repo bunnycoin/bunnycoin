251c2d070e435147c9cf22125f3843cae5c4456d
Michal Malek <mmalek@milosolutions.com>
2019-03-06 14:40:15 +0100

    fix and enable netbase unit tests

--- src/test/netbase_tests.cpp
+++ src/test/netbase_tests.cpp
@@ -14,8 +14,10 @@ BOOST_AUTO_TEST_CASE(netbase_networks)
     BOOST_CHECK(CNetAddr("127.0.0.1").GetNetwork()                              == NET_UNROUTABLE);
     BOOST_CHECK(CNetAddr("::1").GetNetwork()                                    == NET_UNROUTABLE);
     BOOST_CHECK(CNetAddr("8.8.8.8").GetNetwork()                                == NET_IPV4);
+#ifdef USE_IPV6
     BOOST_CHECK(CNetAddr("2001::8888").GetNetwork()                             == NET_IPV6);
     BOOST_CHECK(CNetAddr("FD87:D87E:EB43:edb1:8e4:3588:e546:35ca").GetNetwork() == NET_TOR);
+#endif
 }
 
 BOOST_AUTO_TEST_CASE(netbase_properties)
@@ -26,6 +28,7 @@ BOOST_AUTO_TEST_CASE(netbase_properties)
     BOOST_CHECK(CNetAddr("10.0.0.1").IsRFC1918());
     BOOST_CHECK(CNetAddr("192.168.1.1").IsRFC1918());
     BOOST_CHECK(CNetAddr("172.31.255.255").IsRFC1918());
+#ifdef USE_IPV6
     BOOST_CHECK(CNetAddr("2001:0DB8::").IsRFC3849());
     BOOST_CHECK(CNetAddr("169.254.1.1").IsRFC3927());
     BOOST_CHECK(CNetAddr("2002::1").IsRFC3964());
@@ -35,10 +38,15 @@ BOOST_AUTO_TEST_CASE(netbase_properties)
     BOOST_CHECK(CNetAddr("FE80::").IsRFC4862());
     BOOST_CHECK(CNetAddr("64:FF9B::").IsRFC6052());
     BOOST_CHECK(CNetAddr("FD87:D87E:EB43:edb1:8e4:3588:e546:35ca").IsTor());
+#endif
     BOOST_CHECK(CNetAddr("127.0.0.1").IsLocal());
+#ifdef USE_IPV6
     BOOST_CHECK(CNetAddr("::1").IsLocal());
+#endif
     BOOST_CHECK(CNetAddr("8.8.8.8").IsRoutable());
+#ifdef USE_IPV6
     BOOST_CHECK(CNetAddr("2001::1").IsRoutable());
+#endif
     BOOST_CHECK(CNetAddr("127.0.0.1").IsValid());
 }
 
@@ -82,12 +90,15 @@ BOOST_AUTO_TEST_CASE(netbase_lookupnumeric)
     BOOST_CHECK(TestParse("127.0.0.1", "127.0.0.1:65535"));
     BOOST_CHECK(TestParse("127.0.0.1:9333", "127.0.0.1:9333"));
     BOOST_CHECK(TestParse("::ffff:127.0.0.1", "127.0.0.1:65535"));
+#ifdef USE_IPV6
     BOOST_CHECK(TestParse("::", "[::]:65535"));
     BOOST_CHECK(TestParse("[::]:9333", "[::]:9333"));
+#endif
     BOOST_CHECK(TestParse("[127.0.0.1]", "127.0.0.1:65535"));
     BOOST_CHECK(TestParse(":::", ""));
 }
 
+#ifdef USE_IPV6
 BOOST_AUTO_TEST_CASE(onioncat_test)
 {
     // values from http://www.cypherpunk.at/onioncat/wiki/OnionCat
@@ -98,5 +109,6 @@ BOOST_AUTO_TEST_CASE(onioncat_test)
     BOOST_CHECK(addr1.ToStringIP() == "5wyqrzbvrdsumnok.onion");
     BOOST_CHECK(addr1.IsRoutable());
 }
+#endif
 
 BOOST_AUTO_TEST_SUITE_END()
